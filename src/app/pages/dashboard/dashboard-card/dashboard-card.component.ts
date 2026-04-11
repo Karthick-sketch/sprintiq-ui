02adcf475331148c38a0455ffd08dc8fd809e144
Karthick-sketch <karthick246.kh@outlook.com>
2026-04-11 09:11:13 +0000

    feat: implement dashboard metrics view with reusable card component and updated styling

---
+++ src/app/pages/dashboard/dashboard-card/dashboard-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { IconComponent } from '../../icon/icon.component';
+
+@Component({
+  selector: 'app-dashboard-card',
+  standalone: true,
+  imports: [IconComponent],
+  templateUrl: './dashboard-card.component.html',
+  styleUrl: './dashboard-card.component.css',
+})
+export class DashboardCardComponent {
+  @Input() value: number = 0;
+  @Input() label: string = '';
+  @Input() icon: string = '';
+  @Input() color: string = '';
+  @Input() bgColor: string = '';
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { DashboardCardComponent } from './dashboard-card/dashboard-card.component';
+import { MetricsDTO } from '../../dto/dashboard/metrics.dto';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
+  imports: [DashboardCardComponent],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  metrics: MetricsDTO = {
+    projects: 12,
+    tickets: 28,
+    completed: 42,
+  };
+}
